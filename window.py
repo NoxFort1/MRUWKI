f89414663e0f2e250f4328918848222ea1d0f7e1
NoxFort <bartosz20044@gmail.com>
2022-05-29 13:05:09 +0200

    Ant model, background model, ant's movement

--- window.py
+++ window.py
@@ -4,7 +4,13 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((1600, 800))
 pygame.display.set_caption('Symulator')
-clock = pygame.time.CLock()
+clock = pygame.time.Clock()
+
+background_surface = pygame.image.load('graphics/background.jpg').convert()
+
+ant_surface = pygame.image.load('graphics/antmodel.png').convert_alpha()
+ant_x_pos = 0
+ant_vel = 3
 
 while True:
     for event in pygame.event.get():
@@ -12,6 +18,10 @@ while True:
             pygame.quit()
             exit()
 
-
+    screen.blit(background_surface, (0, 0))
+    screen.blit(ant_surface, (ant_x_pos, 0))
+    ant_x_pos += ant_vel
+    if ant_x_pos >= 1600:
+        ant_x_pos = 0
     pygame.display.update()
     clock.tick(60)
